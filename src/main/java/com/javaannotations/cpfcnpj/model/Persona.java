d7885c0640c2a2db464e80d968d46dfe4b0f5352
Denise Anjos <deniseb.anjos@outlook.com>
2023-04-19 16:10:32 -0300

    Implementacao de interface interceptando ambas anotacoes para validacao de documentos em um unico atributo

--- src/main/java/com/javaannotations/cpfcnpj/model/Persona.java
+++ src/main/java/com/javaannotations/cpfcnpj/model/Persona.java
@@ -3,11 +3,13 @@ package com.javaannotations.cpfcnpj.model;
 import org.hibernate.validator.constraints.br.CNPJ;
 import org.hibernate.validator.constraints.br.CPF;
 
+import com.javaannotations.cpfcnpj.controller.resources.CPFouCNPJ;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,17 +25,20 @@ import lombok.ToString;
 @ToString
 @Entity(name = "db_persona")
 public class Persona {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@CPF(message = "@CPF informado invalido.")
-	@Valid
 	private String cpf;
-	
+
 	@CNPJ(message = "@CNPJ informado invalido.")
-	@Valid
 	private String cnpj;
 
+	@CPFouCNPJ
+	@Pattern(regexp = "([0-9]{11}|[0-9]{14})", message = "CPF deve conter 11 digitos e CNPJ deve conter 14 digitos. Nao e aceito pontuacoes.")
+	@Pattern(regexp = "^(?:(?![0]{11}|[1]{11}|[2]{11}|[3]{11}|[4]{11}|[5]{11}|[6]{11}|[7]{11}|[8]{11}|[9]{11}).)*$", message = "CPF nao deve conter digitos repetidos.")
+	@Pattern(regexp = "^(?:(?![0]{14}|[1]{14}|[2]{14}|[3]{14}|[4]{14}|[5]{14}|[6]{14}|[7]{14}|[8]{14}|[9]{14}).)*$", message = "CNPJ nao deve conter digitos repetidos.")
+	private String documento;
 }
--- src/test/java/com/javaannotations/cpfcnpj/controller/PersonaControllerTest.java
+++ src/test/java/com/javaannotations/cpfcnpj/controller/PersonaControllerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -35,7 +36,7 @@ class PersonaControllerTest {
 	private PersonaService service;
 
 	@Test
-	@DisplayName("")
+	@DisplayName("Teste sucesso documentos validos SEM pontuacao")
 	void testEsperaSucessoAoIncluirDadosValidosSemPontuacao() throws Exception {
 		// teste cenario sucesso com documentos validos sem pontuacao
 		Persona persona = Persona.builder().cpf("43052288318").cnpj("99877404000125").build();
@@ -51,13 +52,37 @@ class PersonaControllerTest {
 		assertNotNull(result.getResponse());
 		assertEquals(201, result.getResponse().getStatus());
 		assertTrue(result.getResponse().getContentAsString().contains("43052288318"));
+		assertTrue(result.getResponse().getContentAsString().contains("99877404000125"));
 		
 	}
 	
 	@Test
-	void testEsperaExceptionAotentarIncluirDadosInvalidos() throws Exception {
-		// teste cenario exception com documentos invalidos sem pontuacao
-		Persona persona = Persona.builder().cpf(null).cnpj("0000000000001").build();
+	@DisplayName("Teste sucesso documentos validos COM pontuacao")
+	void testEsperaSucessoAoIncluirDadosValidosComPontuacao() throws Exception {
+		// teste cenario sucesso com documentos validos com pontuacao
+		Persona persona = Persona.builder().cpf("430.522.883-18").cnpj("99.877.404/0001-25").build();
+		when(service.salvar(any(Persona.class))).thenReturn(persona);
+		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(201))
+			    .andExpect(MockMvcResultMatchers.content().string(convertObjectToJsonString(persona)))
+			    .andReturn();
+		
+		assertNotNull(result.getResponse());
+		assertEquals(201, result.getResponse().getStatus());
+		assertTrue(result.getResponse().getContentAsString().contains("430.522.883-18"));
+		assertTrue(result.getResponse().getContentAsString().contains("99.877.404/0001-25"));
+		
+	}
+	
+	@Test
+	@DisplayName("Teste exception documentos invalidos SEM pontuacao")
+	void BUG_testEsperaExceptionAotentarIncluirDadosInvalidosSemPontuacao() throws Exception {
+		// teste cenario exception com CNPJ invalido sem pontuacao
+		// TODO: BUG pattern hoje aceita todos os digitos 0 no CNPJ "00000000000000"
+		Persona persona = Persona.builder().cnpj("1111111111111").build();
 		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
 				.content(convertObjectToJsonString(persona))
 			    .contentType(MediaType.APPLICATION_JSON)
@@ -68,6 +93,214 @@ class PersonaControllerTest {
 		assertTrue(result.getResolvedException().getMessage().contains("@CNPJ informado invalido."));
 		verifyNoInteractions(service);
 		
+		// teste cenario exception com CNPJ invalido sem pontuacao
+		persona = Persona.builder().cnpj("11111111111111").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(400)).andReturn();
+
+		assertTrue(result.getResolvedException().getMessage().contains("@CNPJ informado invalido."));
+		verifyNoInteractions(service);
+		
+		// teste cenario exception com CPF invalido sem pontuacao
+		persona = Persona.builder().cpf("00000000000").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+		
+		assertTrue(result.getResolvedException().getMessage().contains("@CPF informado invalido."));
+		verifyNoInteractions(service);
+	}
+	
+	@Test
+	@DisplayName("Teste exception documentos invalidos COM pontuacao")
+	void BUG_testEsperaExceptionAotentarIncluirDadosInvalidosComPontuacao() throws Exception {
+		// teste cenario exception com CNPJ invalido com pontuacao
+		// TODO: BUG pattern hoje aceita todos os digitos 0 "00.000.000/0000-00"
+		Persona persona = Persona.builder().cnpj("11.111.111/1111-11").build();
+		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+		
+		assertTrue(result.getResolvedException().getMessage().contains("@CNPJ informado invalido."));
+		verifyNoInteractions(service);
+		
+		// teste cenario exception com CNPJ invalido com pontuacao
+		// TODO: BUG pattern hoje aceita todos os digitos 0
+		persona = Persona.builder().cnpj("11.111.111/1111-11").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(400))
+				.andReturn();
+
+		assertTrue(result.getResolvedException().getMessage().contains("@CNPJ informado invalido."));
+		verifyNoInteractions(service);
+		
+		// teste cenario exception com CPF invalido com pontuacao
+		persona = Persona.builder().cpf("000.000.000-00").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+		
+		assertTrue(result.getResolvedException().getMessage().contains("@CPF informado invalido."));
+		verifyNoInteractions(service);
+	}
+	
+	@Test
+	@DisplayName("Teste metodo inclusao documento valido COM pontuacao e com ambas anotacoes antes da implementacao da regra de negocio")
+	@Disabled("Implementacao de regra que nao permite pontuacao.")
+	void testeEsperaSucessoAoIncluirDadosValidosComPontuacao() throws Exception {
+		// teste cenario passando CPF com pontuacao
+		Persona persona = Persona.builder().documento("430.522.883-18").build();
+		when(service.salvar(any(Persona.class))).thenReturn(persona);
+		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(201))
+			    .andReturn();
+		
+		assertNotNull(result.getResponse());
+		assertEquals(201, result.getResponse().getStatus());
+		assertTrue(result.getResponse().getContentAsString().contains("430.522.883-18"));
+		
+		// teste cenario passando CNPJ com pontuacao
+		persona = Persona.builder().documento("99.877.404/0001-25").build();
+		when(service.salvar(any(Persona.class))).thenReturn(persona);
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(201))
+			    .andReturn();
+				
+		assertNotNull(result.getResponse());
+		assertEquals(201, result.getResponse().getStatus());
+		assertTrue(result.getResponse().getContentAsString().contains("99.877.404/0001-25"));
+	}
+	
+	@Test
+	@DisplayName("Teste exception ao tentar incluir documento valido com pontuacao")
+	void testeEsperaExcecaoAoTentarIncluirDadosValidosComPontuacao() throws Exception {
+		// teste cenario passando CPF com pontuacao
+		Persona persona = Persona.builder().documento("430.522.883-18").build();
+		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+		
+		assertTrue(result.getResolvedException().getMessage().contains("CPF deve conter 11 digitos e CNPJ deve conter 14 digitos. Nao e aceito pontuacoes"));
+		verifyNoInteractions(service);
+		
+		// teste cenario passando CNPJ com pontuacao
+		persona = Persona.builder().documento("99.877.404/0001-25").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+				
+		assertTrue(result.getResolvedException().getMessage().contains("CPF deve conter 11 digitos e CNPJ deve conter 14 digitos. Nao e aceito pontuacoes"));
+		verifyNoInteractions(service);
+	}
+	
+	@Test
+	@DisplayName("Teste exception ao tentar incluir documento invalido SEM pontuacao")
+	void testeEsperaExcecaoAoTentarIncluirDadosInvalidosRepetidos() throws Exception {
+		// teste cenario passando CPF invalido com numeros repetidos
+		Persona persona = Persona.builder().documento("00000000000").build();
+		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+		
+		assertTrue(result.getResolvedException().getMessage().contains("CPF nao deve conter digitos repetidos"));
+		verifyNoInteractions(service);
+		
+		// teste cenario passando CPF invalido com numeros repetidos
+		persona = Persona.builder().documento("11111111111").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+		
+		assertTrue(result.getResolvedException().getMessage().contains("CPF nao deve conter digitos repetidos"));
+		verifyNoInteractions(service);
+		
+		// teste cenario passando CPF invalido com numeros repetidos
+		persona = Persona.builder().documento("00000000000000").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+				
+		assertTrue(result.getResolvedException().getMessage().contains("CNPJ nao deve conter digitos repetidos"));
+		verifyNoInteractions(service);
+		
+		// teste cenario passando CPF invalido com numeros repetidos
+		persona = Persona.builder().documento("11111111111111").build();
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(400))
+			    .andReturn();
+				
+		assertTrue(result.getResolvedException().getMessage().contains("CNPJ nao deve conter digitos repetidos"));
+		verifyNoInteractions(service);
+	}
+	
+	@Test
+	@DisplayName("Teste sucesso ao tentar incluir documento valido SEM pontuacao")
+	void testeEsperaSucessoAoIncluirDadosValidosSemPontuacao() throws Exception {
+		// teste cenario passando CPF sem pontuacao
+		Persona persona = Persona.builder().documento("43052288318").build();
+		when(service.salvar(any(Persona.class))).thenReturn(persona);
+		var result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(201))
+			    .andReturn();
+		
+		assertNotNull(result.getResponse());
+		assertEquals(201, result.getResponse().getStatus());
+		assertTrue(result.getResponse().getContentAsString().contains("43052288318"));
+		
+		// teste cenario passando CNPJ sem pontuacao
+		persona = Persona.builder().documento("99877404000125").build();
+		when(service.salvar(any(Persona.class))).thenReturn(persona);
+		result = mvc.perform(MockMvcRequestBuilders.post("/doc-validator")
+				.content(convertObjectToJsonString(persona))
+			    .contentType(MediaType.APPLICATION_JSON)
+			    .accept(MediaType.APPLICATION_JSON))
+			    .andExpect(MockMvcResultMatchers.status().is(201))
+			    .andReturn();
+				
+		assertNotNull(result.getResponse());
+		assertEquals(201, result.getResponse().getStatus());
+		assertTrue(result.getResponse().getContentAsString().contains("99877404000125"));
 	}
 	
 	//Converts Object to Json String
